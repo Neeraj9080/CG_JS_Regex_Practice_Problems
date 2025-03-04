e5f4b8ca81cd2754ce1231a88dda47242059c66d
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-04 23:53:04 +0530

    [Neeraj] Add. Support optional country code TLD for email validation

--- EmailRegex/emailregex.js
+++ EmailRegex/emailregex.js
@@ -1,7 +1,7 @@
 function validateEmail(email) {
-  const pattern = /^[a-zA-Z0-9_.+-]{3,}@([a-zA-Z0-9-]{3,})\.([a-zA-Z]{2,})$/;
+  const pattern = /^[a-zA-Z0-9_.+-]{3,}@([a-zA-Z0-9-]{3,})\.([a-zA-Z]{2,})(\.[a-zA-Z]{2})?$/;
   return pattern.test(email);
 }
 
-console.log(validateEmail("abc.xyz@bridgelabz.co"));  // true
-console.log(validateEmail("abc@bridgelabz.co"));      // true
+console.log(validateEmail("abc.xyz@bridgelabz.co.in"));  // true
+console.log(validateEmail("abc@bridgelabz.co.in"));      // true
