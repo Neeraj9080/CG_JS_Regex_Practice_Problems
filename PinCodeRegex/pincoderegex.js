b87ffecfafadd18cbdf77ed9df4554789d77a76f
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-04 23:26:49 +0530

    [Neeraj] Add. allowing spaces within the digits

--- PinCodeRegex/pincoderegex.js
+++ PinCodeRegex/pincoderegex.js
@@ -1,6 +1,7 @@
 function validatePIN(pin) {
-    const pattern = /^\d{6}$/;
-    return pattern.test(pin);
-  }
-  
-  console.log(validatePIN("400088B"));  // false
+  const pattern = /^\d{3} \d{3}$|^\d{6}$/;
+  return pattern.test(pin);
+}
+
+console.log(validatePIN("400 088"));  // true
+console.log(validatePIN("400088"));    // true
