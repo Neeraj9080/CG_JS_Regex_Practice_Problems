2ef0d6430ac6c6e2bb66fb39c521d4549c4c26bc
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-04 23:44:15 +0530

    [Neeraj] Add. Validate '.' after 'bridgelabz' and mandatory 'co' in email

--- EmailRegex/emailregex.js
+++ EmailRegex/emailregex.js
@@ -1,7 +1,7 @@
 function validateEmail(email) {
-  const pattern = /^[a-zA-Z]{3,}@([a-zA-Z]{3,})$/;
+  const pattern = /^[a-zA-Z]{3,}@([a-zA-Z]{3,})\.([a-zA-Z]{2,})$/;
   return pattern.test(email);
 }
 
-console.log(validateEmail("abc@bridgelabz"));  // true
-console.log(validateEmail("abcbrid"));          // false
+console.log(validateEmail("abc@bridgelabz.co"));  // true
+console.log(validateEmail("abc@bridgelabz.c"));   // false
