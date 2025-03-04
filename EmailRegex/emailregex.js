b6539cbececf3d7aeeafba7b3e6bc0f3646d96ce
Neeraj9080 <neerajpatel02468@gmail.com>
2025-03-04 23:41:50 +0530

    [Neeraj] Add. Ensure '@' and validate 'bridgelabz' in email

--- EmailRegex/emailregex.js
+++ EmailRegex/emailregex.js
@@ -1,7 +1,7 @@
 function validateEmail(email) {
-  const pattern = /^[a-zA-Z]{3,}$/;
+  const pattern = /^[a-zA-Z]{3,}@([a-zA-Z]{3,})$/;
   return pattern.test(email);
 }
 
-console.log(validateEmail("abc"));  // true
-console.log(validateEmail("ab"));    // false
+console.log(validateEmail("abc@bridgelabz"));  // true
+console.log(validateEmail("abcbrid"));          // false
